83c0cb20eb9ddd0af591605f73c2e3dc5d6821bf
Mandeep Sah <msah@icct.com>
2018-11-07 16:35:16 -0500

    Changing Welcome message

--- samples/sample-springboot/src/main/java/org/mannietes/springboot/controllers/HelloController.java
+++ samples/sample-springboot/src/main/java/org/mannietes/springboot/controllers/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello Hello ICC Openshift!";
+        return "Hello Hello Columbus Openshift!";
     }
 
 }
