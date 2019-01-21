be235a7c828b04025e9a8598e593ec14accd23f0
Mandeep Sah <msah@icct.com>
2019-01-21 15:06:24 -0500

    updating with more nimple steps

--- create-sample.sh
+++ create-sample.sh
@@ -1,3 +1,9 @@
+#Set the environment again
+eval $(minishift oc-env)
+
+eval $(minishift docker-env)
+docker login -u $(oc whoami) -p $(oc whoami -t) $(minishift openshift registry)
+
 #New NODE APP
 oc new-app https://github.com/openshift/nodejs-ex -l name=nodejs-ex-app
 
@@ -11,13 +17,13 @@ minishift openshift registry
 #NEW SPRING BOOT APP 
 cd samples/sample-springboot/ && mvn clean package
 
-docker build . -t $(minishift openshift registry)/test-project/springboot:dev
+docker build . -t $(minishift openshift registry)/dev-spring-project/springboot:dev
 
-docker images | grep sample-springboot
+docker images | grep springboot
 
-docker push $(minishift openshift registry)/test-project/springboot:dev
+docker push $(minishift openshift registry)/dev-spring-project/springboot:dev
 
-oc project test-project
+oc project dev-spring-project
 
 oc new-app --image-stream=springboot:dev --name=springboot
 
@@ -26,3 +32,11 @@ oc expose service springboot
 oc get pods
 
 oc get routes
+
+echo "Starting IT SETUP"
+docker tag $(minishift openshift registry)/dev-spring-project/springboot:dev $(minishift openshift registry)/it-spring-project/springboot:it
+
+oc project it-spring-project
+oc new-app --image-stream=springboot:it --name=springboot
+
+oc expose service springboot
--- environment.sh
+++ environment.sh
@@ -1,23 +1,36 @@
 # This path will need to change based on your local set up
-#minishift oc-env export PATH="/Users/msah/.minishift/cache/oc/v3.10.0/darwin:$PATH" 
+echo 'Setting minishift env: There are a few sleeps, so it will take few minutes for this script to complete'
+minishift oc-env export PATH="/Users/msah/.minishift/cache/oc/v3.10.0/darwin:$PATH" 
+echo '$(minishift oc-env)'
 eval $(minishift oc-env)
 
-
+echo 'Sleeping for 60 seconds'
 sleep 60
-#minishift docker-env export DOCKER_TLS_VERIFY="1" export DOCKER_HOST="tcp://192.168.64.2:2376" export DOCKER_CERT_PATH="/Users/msah/.minishift/certs"
+
+echo 'minishift docker-env export'
+minishift docker-env export DOCKER_TLS_VERIFY="1" export DOCKER_HOST="tcp://$(minishift ip):2376" export DOCKER_CERT_PATH="/Users/msah/.minishift/certs"
+
+echo '$(minishift docker-env)'
 eval $(minishift docker-env)
 
+echo 'Sleeping for 60 seconds'
 sleep 60
+
+echo 'OC USer Setup'
 #oc login -u test-admin
 oc login -u system:admin
 
-oc adm policy add-cluster-role-to-user cluster-admin mini-admin --as=system:admin
+#echo "User name is: 'developer'"
+oc adm policy add-cluster-role-to-user cluster-admin developer --as=system:admin
 
-oc login -u mini-admin
+oc login -u developer
 oc new-project test-project
+oc new-project dev-spring-project
+oc new-project it-spring-project
 
 echo $(minishift openshift registry)
+echo 'printing docker info'
 docker info 
-
+echo 'ending printing docker info'
 
 docker login -u $(oc whoami) -p $(oc whoami -t) $(minishift openshift registry)
